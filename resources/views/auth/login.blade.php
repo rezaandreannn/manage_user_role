7c1ca575c2d84df9b78d2c66686675b349e7bdc6
rezaandreannn <andreanreza042@gmail.com>
2024-01-06 10:51:36 +0700

    delete href sign up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,45 +50,45 @@
                                     <div class="d-flex justify-content-center">
                                         <button type="submit" class="btn btn-primary">{{ __('Sign In') }}</button>
                                     </div>
-                                    <p class="text-center my-3">or sign in with other accounts?</p>
+                                    {{-- <p class="text-center my-3">or sign in with other accounts?</p>
                                     <div class="d-flex justify-content-center">
                                         <ul class="list-group list-group-horizontal list-group-flush">
                                             <li class="list-group-item border-0 pb-0">
                                                 <a href="#"><img src="{{asset('images/brands/fb.svg')}}" alt="fb"></a>
-                                            </li>
-                                            <li class="list-group-item border-0 pb-0">
-                                                <a href="#"><img src="{{asset('images/brands/gm.svg')}}" alt="gm"></a>
-                                            </li>
-                                            <li class="list-group-item border-0 pb-0">
-                                                <a href="#"><img src="{{asset('images/brands/im.svg')}}" alt="im"></a>
-                                            </li>
-                                            <li class="list-group-item border-0 pb-0">
-                                                <a href="#"><img src="{{asset('images/brands/li.svg')}}" alt="li"></a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <p class="mt-3 text-center">
-                                        Don’t have an account? <a href="{{route('auth.signup')}}" class="text-underline">Click here to sign up.</a>
-                                    </p>
-                                </form>
+                                    </li>
+                                    <li class="list-group-item border-0 pb-0">
+                                        <a href="#"><img src="{{asset('images/brands/gm.svg')}}" alt="gm"></a>
+                                    </li>
+                                    <li class="list-group-item border-0 pb-0">
+                                        <a href="#"><img src="{{asset('images/brands/im.svg')}}" alt="im"></a>
+                                    </li>
+                                    <li class="list-group-item border-0 pb-0">
+                                        <a href="#"><img src="{{asset('images/brands/li.svg')}}" alt="li"></a>
+                                    </li>
+                                    </ul>
                             </div>
+                            <p class="mt-3 text-center">
+                                Don’t have an account? <a href="{{route('auth.signup')}}" class="text-underline">Click here to sign up.</a>
+                            </p> --}}
+                            </form>
                         </div>
                     </div>
                 </div>
-                <div class="sign-bg">
-                    <svg width="280" height="230" viewBox="0 0 431 398" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g opacity="0.05">
-                            <rect x="-157.085" y="193.773" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 -157.085 193.773)" fill="#3B8AFF" />
-                            <rect x="7.46875" y="358.327" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 7.46875 358.327)" fill="#3B8AFF" />
-                            <rect x="61.9355" y="138.545" width="310.286" height="77.5714" rx="38.7857" transform="rotate(45 61.9355 138.545)" fill="#3B8AFF" />
-                            <rect x="62.3154" y="-190.173" width="543" height="77.5714" rx="38.7857" transform="rotate(45 62.3154 -190.173)" fill="#3B8AFF" />
-                        </g>
-                    </svg>
-                </div>
             </div>
-            <div class="col-md-6 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden">
-                <img src="{{asset('images/auth/01.png')}}" class="img-fluid gradient-main animated-scaleX" alt="images">
+            <div class="sign-bg">
+                <svg width="280" height="230" viewBox="0 0 431 398" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.05">
+                        <rect x="-157.085" y="193.773" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 -157.085 193.773)" fill="#3B8AFF" />
+                        <rect x="7.46875" y="358.327" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 7.46875 358.327)" fill="#3B8AFF" />
+                        <rect x="61.9355" y="138.545" width="310.286" height="77.5714" rx="38.7857" transform="rotate(45 61.9355 138.545)" fill="#3B8AFF" />
+                        <rect x="62.3154" y="-190.173" width="543" height="77.5714" rx="38.7857" transform="rotate(45 62.3154 -190.173)" fill="#3B8AFF" />
+                    </g>
+                </svg>
             </div>
+        </div>
+        <div class="col-md-6 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden">
+            <img src="{{asset('images/auth/01.png')}}" class="img-fluid gradient-main animated-scaleX" alt="images">
+        </div>
         </div>
     </section>
 </x-guest-layout>
